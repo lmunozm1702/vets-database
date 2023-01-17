7ef3a895db01c8d192905c4ef4aa893d1bcd0abe
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-17 14:34:30 -0300

    Update queries.sql
    Include queries asked on 3rd. delivery requirements.

--- queries.sql
+++ queries.sql
@@ -181,3 +181,79 @@ WHERE
     date_of_birth BETWEEN '1990/01/01' AND '2000/12/31'
 GROUP BY
     species;
+
+/** Write queries (using JOIN) to answer the following questions: **/
+/* What animals belong to Melody Pond? */
+SELECT
+    animals.name
+FROM
+    animals
+    INNER JOIN owners ON (animals.owner_id = owners.id)
+WHERE
+    owners.full_name = 'Melody Pond';
+
+/* List of all animals that are pokemon (their type is Pokemon). */
+SELECT
+    animals.name
+from
+    animals
+    INNER JOIN species ON (animals.species_id = species.id)
+WHERE
+    species.name = 'Pokemon';
+
+/* List all owners and their animals, remember to include those that don't own any animal. */
+SELECT
+    owners.full_name,
+    animals.name
+FROM
+    owners
+    LEFT JOIN animals ON (owners.id = animals.owner_id);
+
+/* How many animals are there per species? */
+SELECT
+    species.name,
+    COUNT(*)
+FROM
+    species
+    INNER JOIN animals ON (animals.species_id = species.id)
+GROUP BY
+    species.name;
+
+/* List all Digimon owned by Jennifer Orwell. */
+SELECT
+    animals.name,
+    species.name,
+    owners.full_name
+FROM
+    animals
+    INNER JOIN species ON (animals.species_id = species.id)
+    INNER JOIN owners ON (animals.owner_id = owners.id)
+WHERE
+    owners.full_name = 'Jennifer Orwell'
+    AND species.name = 'Digimon';
+
+/* List all animals owned by Dean Winchester that haven't tried to escape. */
+SELECT
+    animals.name,
+    animals.escape_attempts,
+    owners.full_name
+FROM
+    animals
+    INNER JOIN owners ON (animals.owner_id = owners.id)
+WHERE
+    owners.full_name = 'Dean Winchester'
+    AND animals.escape_attempts = 0;
+
+/* Who owns the most animals? */
+SELECT
+    owners.full_name,
+    COUNT(*)
+FROM
+    animals
+    INNER JOIN owners ON (animals.owner_id = owners.id)
+GROUP BY
+    owners.full_name
+ORDER BY
+    COUNT(*) DESC
+LIMIT
+    1;
